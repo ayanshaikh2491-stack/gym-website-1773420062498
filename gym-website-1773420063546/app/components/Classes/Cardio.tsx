2aa3a35800407fff0a3e1561ef8602d99d485817
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 22:11:06 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773420063546/app/components/Classes/Cardio.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Cardio.module.css';
+
+function CardioPage() {
+  return (
+    <div className={styles.container}>
+      <h1>Cardio Classes</h1>
+      <p>Get your heart rate up with our high-intensity cardio classes.</p>
+    </div>
+  );
+}
+
+export default CardioPage;
